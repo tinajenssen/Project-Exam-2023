b23d5e23f7f5a11510ba684171eb64acd77d5024
tinajenssen <63742433+tinajenssen@users.noreply.github.com>
2023-04-11 12:22:31 +0200

    userprofile

--- src/components/userprofile/UserprofilePage.jsx
+++ src/components/userprofile/UserprofilePage.jsx
@@ -3,17 +3,13 @@ import Container from "react-bootstrap/Container";
 
 import Navigation from "../layout/Navigation";
 import Footer from "../layout/Footer";
-import GetUserprofile from "../../js/api/user/GetUserprofile";
 
 export default function UserProfile() {
   return (
     <>
       <Navigation />
 
-      <Container fluid className="container__profilepage pb-5">
-        <h1>get user profile</h1>
-        <GetUserprofile />
-      </Container>
+      <Container fluid className="container__profilepage pb-5"></Container>
 
       <Footer />
     </>
